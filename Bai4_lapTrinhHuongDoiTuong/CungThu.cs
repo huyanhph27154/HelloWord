b29112b4d0bccf47fd5075afb04836e1b2a3c223
admin <admin@LAPTOP-P7KIH4O2>
2024-01-19 08:59:13 +0700

    Add project files.

---
+++ Bai4_lapTrinhHuongDoiTuong/CungThu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4_lapTrinhHuongDoiTuong
+{
+    public class CungThu
+    {
+        public int DiemTanCong { get; set; }
+        public int DiemPhongThu { get; set; }
+        public int HP { get; set; }
+        public int TocDo { get; set; }
+
+        public CungThu()
+        {
+            
+        }
+
+        public void KyNang1()
+        {
+            this.DiemTanCong *= 2;
+        }
+    }
+}
---
+++ Bai4_lapTrinhHuongDoiTuong/SanPham.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4_lapTrinhHuongDoiTuong
+{
+    internal class SanPham
+    {
+        public int MaSanPham {  get; set; }
+        public string TenSanPham { get; set; }
+        public int DonGia {  get; set; }
+        public int SoLuong {  get; set; }
+
+        public SanPham()
+        {
+            
+        }
+
+        public SanPham(int maSanPham, string tenSanPham, int donGia, int soLuong)
+        {
+            MaSanPham = maSanPham;
+            TenSanPham = tenSanPham;
+            DonGia = donGia;
+            SoLuong = soLuong;
+        }
+    }
+}
